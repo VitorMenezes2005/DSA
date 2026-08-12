edcfec5699a0a3e5a0e083ae13cd03bcac01968a
Vitor Menezes <vitorcarlos2005@outlook.com>
2026-08-12 11:26:13 -0300

    Resolution leetCode 1342 -> new commit

--- src/services/binarios/LeetCode1342.java
+++ src/services/binarios/LeetCode1342.java
@@ -1,4 +1,17 @@
 package services.binarios;
 
 public class LeetCode1342 {
+    public static int numberOfSteps(int num) {
+        int steps = 0;
+
+        while (num > 0) {
+            if ((num & 1) == 1) {
+                num -= 1;
+            } else {
+                num >>= 1;
+            }
+            steps += 1;
+        }
+        return steps;
+    }
 }
